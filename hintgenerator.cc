40c3e4c6526d2d149c8279011c3b4ed6a4b490aa
enllwx <2045198910qq@gmail.com>
2018-08-16 11:31:11 +0800

    ok

--- hintgenerator.cc
+++ hintgenerator.cc
@@ -1,105 +1,283 @@
 #include "hintgenerator.h"
 #include "grid.h"
 #include <iostream>
+#include <vector>
+#include <math.h>
 using namespace std;
-HintGenerator::HintGenerator(shared_ptr <Difficulty> levels,
-                             vector <vector <Cell>> * board,
-                             Block currentBlock):
-  block{currentBlock}, theLevel{levels}, board{board}
-{ memoisations.resize(WIDTH * HEIGHT * 4 + 1); }
 
-Cell HintGenerator::getMemoisation(int row, int col, int rotation){
-  return memoisations[(WIDTH * row + col)*4 + rotation];
-}
 
-Cell HintGenerator::setMemoisation(int row, int col, int rotation, Cell c){
-  return memoisations[(WIDTH * row + col)*4 + rotation] = c;
-}
+void printBoard(vector <vector <Cell>> board);
 
-bool HintGenerator::isMemoised(int row, int col, int rotation){
-  return memoisations[(WIDTH * row + col)*4 + rotation].content
-    != Content::Empty;
-}
-
-// Returns the score of how good a hint a cell is
-double HintGenerator::score(Cell c){
-  Block b = block;
-  while(b.getRotation() != c.rotation)
-    b = theLevel->rotateClock(b);
-
-  double s = c.row - b.getHeight();
-  
-  if((b.getShape() == Shape::Jblock || b.getShape() == Shape::Lblock) &&
-     c.rotation != 0) s -= 0.5;
-  
-  return s;
-}
-
-// Generates the hint
-Block HintGenerator::generateHint(){
-  Cell c = generateHint(block); // Get where the hint is  
-  Block b = Block(block.getShape());
-  
-  while(b.getRotation() != c.rotation) // Set hint block rotation    
-  b = theLevel->rotateClock(b);
-  
-  b.setRow(c.row); b.setCol(c.col); // Set hint block location
-  
-  return b;
-}
-
-// Returns cell, containing the best place to place the given Block b
-// Best place is defined as the row which is nearest
-// the bottom that the Block b can get to
-Cell HintGenerator::generateHint(Block b, int depth){
-  int b_Row = b.getRow();
-  int b_Col = b.getCol();
-  int b_Rotation = b.getRotation();
-  std::vector<Cell> area = b.getArea();
-  Cell temp = Cell{Content::Hint, b_Row, b_Col, b_Rotation};
+Block HintGenerator::moveByWeight(Block b){
+    Block moved = b;
+    moved.setRow(b.getRow()+b.getWeight());
     
-  // If current spot with current rotation visited...
-  if(isMemoised(b_Row, b_Col, b_Rotation) ){
-    return getMemoisation(b_Row, b_Col, b_Rotation);
-  }
+    if(validPosition(moved)) return moved;
+    return b;
+}
+
+
+Block HintGenerator::cwRotate(Block b){
+    int width = b.getWidth();
+    Block rotated = b;
     
-  // If cannot move
-  if(b_Row >= HEIGHT-1){
-    return setMemoisation(b_Row, b_Col, b_Rotation, temp);
-  }
+    std::vector<Cell> oldArea = b.getArea();
+    std::vector<Cell> newArea;
+    for(int i=0; i<4; ++i)
+        // Following 3 lines is the key that makes the rotation work!
+        //   First we find the smartest rectangle that contains the block;
+        //   then rotate the rectangle without cells that have not be covered
+        newArea.emplace_back(Cell{oldArea[i].content,
+            (b.getRow() - width + 1) + (oldArea[i].col - b.getCol()),
+            b.getCol() + (b.getRow() - oldArea[i].row)});
     
-  for(int i=0; i<4; ++i){
-    int row = area[i].row;
-    int col = area[i].col;
-    if((*board)[row+1][col].content != Content::Empty &&
-       !(*board)[row+1][col].temp){
-      return setMemoisation(b_Row, b_Col, b_Rotation, temp);
+    rotated.setArea(newArea);
+    rotated.setRotation((b.getRotation()+1) % 4);
+    
+    if(validPosition(rotated)) return rotated;
+    return b;
+}
+
+
+Block HintGenerator::rotateClock(Block b){
+    return moveByWeight(cwRotate(b));
+}
+
+
+Block HintGenerator::rotateCounter(Block b){
+    return moveByWeight(cwRotate(cwRotate(cwRotate(b))));
+}
+
+
+Block HintGenerator::moveDown(Block b){
+    Block moved = b;
+    moved.setRow(b.getRow()+1);
+    
+    if(validPosition(moved)) return moveByWeight(moved);
+    return moveByWeight(b);
+}
+
+
+Block HintGenerator::moveLeft(Block b){
+    Block moved = b;
+    moved.setCol(b.getCol()-1);
+    
+    if(validPosition(moved)) return moveByWeight(moved);
+    return moveByWeight(b);
+}
+
+
+Block HintGenerator::moveRight(Block b){
+    Block moved = b;
+    moved.setCol(b.getCol()+1);
+    
+    if(validPosition(moved)) return moveByWeight(moved);
+    return moveByWeight(b);
+}
+
+///////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////
+
+
+bool HintGenerator::visited(Block b) const
+{  cout << !visit[b.getRow()][b.getCol()][b.getRotation()] << endl;  return visit[b.getRow()][b.getCol()][b.getRotation()];  }
+
+
+
+bool HintGenerator::validPosition(Block newB) const
+{
+    vector<Cell> area = newB.getArea();
+    
+    for(int i=0; i<4; ++i){
+        int row = area[i].row;
+        int col = area[i].col;
+        
+        if (row < 0 || row >= HEIGHT || col < 0 || col >= WIDTH) return false;
+        if ( !board[row][col].temp
+            && board[row][col].content != Content::Empty ) return false;
+    } return true;
+}
+
+
+
+int HintGenerator::lineEliminate(Block b) const
+{
+    vector <int> RowCounter;
+    for (auto Bcell : b.getArea()){
+        int row = Bcell.row;
+
+        int noElimination = WIDTH;
+        for (int col = 0; col<WIDTH; col++){
+            if ( (board[row][col]).content == Content::Empty ) break;
+            else noElimination--;
+        }
+        
+        bool addNewLine = true;
+        if ( !noElimination ) {
+            for (auto checkedRow : RowCounter){
+                if (checkedRow == row) addNewLine = false;
+            }
+            if ( addNewLine ) RowCounter.push_back(row);
+        }
     }
-  }
-    
-  // Mark current as visited
-  Cell best = setMemoisation(b_Row, b_Col, b_Rotation, temp);
-    
-  // If can move, try each move
-  Cell potential = generateHint(theLevel->moveDown(b), depth+1);
-  if (score(potential) > score(best)) best = potential;
-    
-  potential = generateHint(theLevel->moveLeft(b), depth+1);
-  if (score(potential) > score(best)) best = potential;
-    
-  potential = generateHint(theLevel->moveRight(b), depth+1);
-  if (score(potential) > score(best)) best = potential;
-    
-  potential = generateHint(theLevel->rotateClock(b), depth+1);
-  if (score(potential) > score(best)) best = potential;
-    
-  potential = generateHint(theLevel->rotateCounter(b), depth+1);
-  if (score(potential) > score(best)) best = potential;
-    
-  // Memoise and continue
-  return setMemoisation(b_Row, b_Col, b_Rotation, best);
+    return RowCounter.size();
 }
 
 
+
+// if there is no hole, it will return -1
+int HintGenerator::aboveThehole(Block b) const{
+    
+    int aboveTheHole = -1;
+    for (auto Bcell : b.getArea()){
+        int lineCounter = 0;
+        
+        int row = Bcell.row;
+        int col = Bcell.col;
+        while (row >= 0) {
+            if ( (board[row][col]).content == Content::Empty )
+            { lineCounter = 0; aboveTheHole += lineCounter; break; }
+            row++;
+        }
+    }
+    return aboveTheHole;
+}
+
+
+int HintGenerator::touchThefloot(Block b) const
+{
+    int counter = 0;
+    vector <Cell> area = b.getArea();
+    for (int i=0; i<4; i++){
+        if (area[i].row == HEIGHT-1) counter++;
+    }
+    return counter;
+}
+
+
+
+int HintGenerator::lineCounter(Block b) const
+{
+    int counter = 0;
+    for (auto Bcell : b.getArea()){
+        counter += Bcell.row;
+    }
+    return counter;
+}
+
+
+
+int HintGenerator::underEdges(Block b) const
+{
+    int counter = 0;
+    for (auto Bcell : b.getArea()){
+        int row = Bcell.row;
+        int col = Bcell.col;
+        if (col == 0 ||
+            (board[row][col]).content != Content::Empty )
+            counter++;
+    }
+    return counter;
+}
+
+
+
+float HintGenerator::scoreCounter(Block b) const
+{
+    float score = 0;
+    /*
+    score += lineEliminate(b) * 0.5;
+    score += underEdges(b) * 0.7;
+    score += lineCounter(b) * 0.4;
+    score += touchThefloot(b) * 0.4;
+    int ATH = aboveThehole(b);
+    if (ATH == -1){ score+=1; }
+    else if (ATH == 0){ score-=0.7; }
+    else if (ATH > 0){ score-= pow(0.7, ATH); }
+    return score;
+     */
+    score += lineEliminate(b) * A;
+    score += underEdges(b) * B;
+    score += lineCounter(b) * C;
+    score += touchThefloot(b) * D;
+    int ATH = aboveThehole(b);
+    if (ATH == -1){ score+=E; }
+    else if (ATH == 0){ score-=F; }
+    else if (ATH > 0){ score-= pow(G, ATH); }
+    return score;
+}
+
+
+
+void HintGenerator::betterHint(Block b)
+{
+    int row = b.getRow();
+    int col = b.getCol();
+    int rot = b.getRotation();
+    
+    if ( visit[row][col][rot] ) return;
+    visit[row][col][rot] = 1;
+    int tempScore = scoreCounter(b);
+    
+    ////////////////////////////////////////////////////
+    cout << endl;
+    cout << endl;
+    cout << b;
+    cout << row << "  " << col <<endl;
+    cout << hintGrade << " (hintGrade)  vs  " << col << " (tempGrade)"<<endl;
+    cout << endl;
+    ////////////////////////////////////////////////////
+    
+    if ( tempScore >= hintGrade ){
+        hintGrade = tempScore;
+        hintBlock = b;
+    }
+    // "left "
+    Block left = moveLeft(b); betterHint(left);
+    // "right "
+    Block right = moveRight(b); betterHint(right);
+    // "drown "
+    Block down = moveDown(b); betterHint(down);
+    // "clock "
+    Block clock = rotateClock(b); betterHint(clock);
+    
+    ////////////////////////////////////////////////////////
+    for (auto row : visit){
+        for (auto col : row){
+            int sum = 0;
+            for (auto rot: col){
+                sum = sum + rot;
+            }
+            cout << sum << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+    cout << endl;
+    ////////////////////////////////////////////////////////
+}
+
+
+
+HintGenerator::HintGenerator(Block b, vector<vector<Cell>> board)
+    : board{board}
+{
+    for(int row = 0; row < HEIGHT; ++row){
+        vector <vector<bool>> temp;
+        for(int col = 0; col < WIDTH; ++col){
+            vector <bool> temppp;
+            temppp.push_back(0);
+            temppp.push_back(0);
+            temppp.push_back(0);
+            temppp.push_back(0);
+            temp.emplace_back(temppp);
+        }
+        visit.emplace_back(temp);
+    }
+    betterHint(b);
+}
+
+Block HintGenerator::getHint(){ return hintBlock; }
 
 
--- hintgenerator.h
+++ hintgenerator.h
@@ -7,42 +7,68 @@
 #include <vector>
 #include <memory>
 
+/* ==-------------------------- Algorithm --------------------------== */
+//                                                                     //
+//    In this class, AI was used to determine the current best move,   //
+//  there are several index that will be counted:                      //
+//                                                                     //
+//    1. line the hint block will help eliminated.                     //
+//                                                                     //
+//    2. how many small edge was attached by other blocks or           //
+//       boundary of the game board                                    //
+//                                                                     //
+//    3. if there will be hole under the hint block                    //
+//       (a) if there are no hold we add score                         //
+//       (b) if there are hole right behand, we decrease the score     //
+//       (c) if the hole is not right behand the hint block,           //
+//           as the distances between closed hold and the hint block   //
+//           increasing, we decrease the score by smaller amount       //
+//                                                                     //
+/* ==----------------------------------------------------------------== */
 
 // forward declaration avoiding cyclic dependency
 class Grid;
 
 
-class HintGenerator{
-  // block to try for generating the hint
-  Block block;
-  // the smart pointer to give the dificulty of the level for using the difficulty right now
-  std::shared_ptr <Difficulty> theLevel;
-  // The board to observe
-  std::vector <std::vector <Cell>> * board;
+class HintGenerator {
+    float hintGrade = 0;
+    Block hintBlock;
+    std::vector <std::vector <std::vector <bool>>> visit;
+    std::vector <std::vector <Cell>> board;
     
-  static const int MAX_DEPTH = 1024;
-  // The array used for DP memoisation. Basically a huge vector of cells used for
-  // memoisation. Bad, but better alogrithm is hard, so no.
-  std::vector<Cell> memoisations;
+    Block moveByWeight(Block b);
+    Block cwRotate(Block b);
+    Block rotateClock(Block b);
+    Block rotateCounter(Block b);
+    Block moveDown(Block b);
+    Block moveLeft(Block b);
+    Block moveRight(Block b);
     
-  // Helper memoisation functions
-  Cell getMemoisation(int row, int col, int rotation);
-  Cell setMemoisation(int row, int col, int rotation, Cell c);
-  bool isMemoised(int row, int col, int rotation);
-
-  double score(Cell c);          // Generates the score of a given cell to palce the block
+    bool visited(Block b) const;         // check the given block has been check or not
     
-  Cell generateHint(Block b, int depth = 0); // Generates hint for block at position given by Block b
- public:
-  // ctor
-  HintGenerator(std::shared_ptr <Difficulty> levels,
-                std::vector <std::vector <Cell>> * board,
-                Block currentBlock);    
+    bool validPosition(Block newB) const;
+                                         // check if current block is at valid position
     
-  // Returns cell, containing the best place to place the given Block b
-  // Best place is defined as the row which is nearest the bottom that the Block b can get to
-  Block generateHint();        // Generates hint based on Block block (the class variable)
+    int lineEliminate(Block b) const;    // return how many lines could current block elimimate
+    
+    int aboveThehole(Block b) const;     // run only when there is hole under the block,
+                                            //    give the position of how many lines between
+                                            //    the hold and the block
+    
+    int touchThefloot(Block b) const;    // determine how many cells touch the end of board
+    
+    int lineCounter(Block b) const;      // count the total line number of current block
+    
+    int underEdges(Block b) const;       // determine how many cells of the block has
+                                            //    something under it
+    float scoreCounter(Block b) const;   // evaluate current score for current hint block
+    
+    void betterHint(Block b);            // change current hintBlock to better option
+public:
+    HintGenerator(Block b, std::vector <std::vector <Cell>> board);
+    Block getHint();
 };
 
 #endif
+
 
